36743ee09617aaa67603b53ddb92b4c741fc99f6
igarash1 <igarashi.kouki+git@gmail.com>
2024-07-15 19:22:23 +0900

    Fixes window size

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,10 +9,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import java.math.BigDecimal
 
 @Composable
@@ -149,7 +152,14 @@ fun BigDecimal.addDigit(digit: Int) = this.multiply(BigDecimal(10)).plus(BigDeci
 
 fun main() =
     application {
-        Window(onCloseRequest = ::exitApplication, title = "Simple Calculator") {
+        val state = rememberWindowState(
+            position = WindowPosition(Alignment.Center), size = DpSize(300.dp, 330.dp)
+        )
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "Simple Calculator",
+            state = state,
+        ) {
             App()
         }
     }
